e6644cda449fa325799911caca2071c90bad52ee
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2023-09-25 17:33:58 -0300

    feat::sparkles: new prop in button

--- src/components/ShadCN/ui/button.tsx
+++ src/components/ShadCN/ui/button.tsx
@@ -2,6 +2,7 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 
+import { ButtonHTMLAttributes } from 'react';
 import { cn } from '../lib/utils';
 
 const buttonVariants = cva(
@@ -11,10 +12,12 @@ const buttonVariants = cva(
 			variant: {
 				default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
 				destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
-				outline: 'border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground',
+				outline: 'border border-input text-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground',
 				secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
 				ghost: 'hover:bg-accent hover:text-accent-foreground',
 				link: 'text-primary underline-offset-4 hover:underline',
+				defaultOutlined:
+					'bg-transparent text-primary shadow hover:bg-primary/90 hover:border-transparent hover:text-primary-foreground border border-primary',
 			},
 			size: {
 				default: 'h-9 px-4 py-2',
@@ -30,7 +33,7 @@ const buttonVariants = cva(
 	},
 );
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
 	asChild?: boolean;
 }
 
