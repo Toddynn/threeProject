8edfd18aef9e738aafa269d3311b73ae7ea6f1bd
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2023-09-25 17:32:27 -0300

    feat::sparkles: Create new component Modal

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,13 @@
+import ModalAction from './ModalAction';
+import ModalActions from './ModalActions';
+import ModalContent from './ModalContent';
+import ModalIcon from './ModalIcon';
+import ModalRoot from './ModalRoot';
+
+export const Modal = {
+	Root: ModalRoot,
+	Content: ModalContent,
+	Actions: ModalActions,
+	Action: ModalAction,
+	Icon: ModalIcon,
+};
