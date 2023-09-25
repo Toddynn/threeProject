f31d276177c8f6f1a58dd97c2d5d8270f0dfc8ff
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2023-09-25 17:35:06 -0300

    feat::sparkles: Creating a base layout (poop)

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,173 @@
-import { Loader, OrbitControls } from '@react-three/drei';
+import { Backdrop } from '@mui/material';
+import { Environment } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
-import { ControlTable } from './components/Controls/ControlTable';
-import { ExperienceTable } from './components/Experiences/ExperienceTable';
+import { BiChevronLeft } from 'react-icons/bi';
+import { ExperienceDoor } from './components/Experiences/ExperienceDoor';
+import { Modal } from './components/Modal';
 import { Button } from './components/ShadCN/ui/button';
 
 export default function App() {
 	const [active, setActive] = useState<boolean>(false);
+	const [showDetails, setShowDetails] = useState(false);
+
+	const toggleDetails = () => {
+		setShowDetails(!showDetails);
+	};
+
+	const products = [
+		{
+			image: 'images/fundinho.png',
+			category: 'porta',
+		},
+		{
+			image: 'images/fundinho.png',
+			category: 'porta',
+		},
+		{
+			image: 'images/fundinho.png',
+			category: 'porta',
+		},
+		{
+			image: 'images/fundinho.png',
+			category: 'porta',
+		},
+		{
+			image: 'images/fundinho.png',
+			category: 'porta',
+		},
+		{
+			image: 'images/fundinho.png',
+			category: 'porta',
+		},
+		{
+			image: 'images/fundinho.png',
+			category: 'rodape',
+		},
+		{
+			image: 'images/fundinho.png',
+			category: 'rodape',
+		},
+		{
+			image: 'images/fundinho.png',
+			category: 'rodape',
+		},
+		{
+			image: 'images/fundinho.png',
+			category: 'rodape',
+		},
+		{
+			image: 'images/fundinho.png',
+			category: 'rodape',
+		},
+		{
+			image: 'images/fundinho.png',
+			category: 'rodape',
+		},
+		{
+			image: 'images/fundinho.png',
+			category: 'porta',
+		},
+		{
+			image: 'images/fundinho.png',
+			category: 'porta',
+		},
+		{
+			image: 'images/fundinho.png',
+			category: 'porta',
+		},
+	];
+
+	const portas = products.filter((product) => product.category === 'porta');
+	const rodapes = products.filter((product) => product.category === 'rodape');
+
+	console.log(active);
 
 	return (
-		<div className="relative flex h-screen items-center justify-center ">
-			<div className="outline-muted-foreground relative flex w-[70%] flex-col items-center overflow-hidden rounded-2xl outline lg:h-[650px] lg:flex-row">
-				<ControlTable hidden={!active} path="models/table/Table.gltf" />
-				<div className="flex h-full w-full flex-1 cursor-pointer">
-					<Canvas shadows camera={{ position: [0, 0, 0], fov: 35 }}>
-						<ExperienceTable name="table" active={active} setActive={setActive} portalArgs={[10, 10, 10]} />
-						<OrbitControls enablePan={false} makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2} maxDistance={5} />
-					</Canvas>
-				</div>
-				<div className="flex h-full w-full flex-1 bg-slate-900">
-					adicionar ao carrinho e bla bla bla bla aq
-					<Button>COMPRAR </Button>
+		<div className="relative flex h-screen items-center justify-center scroll-smooth bg-[url(/images/fundinho.png)] bg-cover bg-no-repeat">
+			<div
+				className={` ${
+					showDetails ? 'hidden' : 'block'
+				} relative flex h-[800px] w-[85%] flex-col items-center overflow-hidden rounded-2xl bg-slate-400 outline outline-muted-foreground lg:flex-row`}
+			>
+				<div className="scroll scrollStyled flex h-full w-full flex-1 cursor-pointer flex-col gap-3 overflow-auto  border">
+					<div className="flex w-full gap-4">
+						{portas.map((porta, index) => {
+							return (
+								<div key={index} onClick={toggleDetails}>
+									<div className="h-32 w-32">
+										<img src={porta.image} alt="asd" className="h-full w-full object-cover" />
+									</div>
+								</div>
+							);
+						})}
+					</div>
+					<div className="flex w-full gap-4">
+						{rodapes.map((rodape, index) => {
+							return (
+								<div key={index} onClick={toggleDetails}>
+									<div className="h-32 w-32">
+										<img src={rodape.image} alt="asd" className="h-full w-full object-cover" />
+									</div>
+								</div>
+							);
+						})}
+					</div>
 				</div>
 			</div>
-			{/* <Canvas shadows camera={{ position: [0, 0, 10], fov: 30 }}>
-				<ExperienceCharacters />
-			</Canvas>
-			<ControlAnimations /> */}
-			<Loader />
+
+			<AnimatePresence>
+				{showDetails && (
+					<Backdrop
+						component={'div'}
+						open={showDetails}
+						onClick={(e: any) => {
+							e.stopPropagation();
+							setShowDetails(false);
+							setActive(false);
+						}}
+					>
+						<Modal.Root
+							onClick={(e: any) => e.stopPropagation()}
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+							transition={{ duration: 0.3 }}
+							className="relative flex h-[800px] w-[90%] items-center overflow-hidden rounded-md bg-gray-600/40 outline outline-2 outline-white/30 backdrop-blur-md"
+						>
+							<Modal.Content className="scrollStyled flex h-full w-full flex-col overflow-auto lg:flex-row">
+								<div className="relative flex h-full w-full flex-1 flex-col justify-between bg-[#c1bcb4]">
+									<Canvas shadows camera={{ position: [-15, 0, 0], fov: 20, castShadow: true, receiveShadow: true }}>
+										<ambientLight intensity={1} />
+										<Environment preset={'sunset'} />
+										<ExperienceDoor name="door" active={active} preset="city" portalArgs={[10, 10, 10]} />
+									</Canvas>
+									<Modal.Action className="absolute bottom-5 left-5 justify-start">
+										<Button onClick={() => setActive(!active)} className="w-auto">
+											{active ? 'Fechar visualização' : 'Visualizar 3D'}
+										</Button>
+									</Modal.Action>
+								</div>
+
+								<div className={` flex h-full w-full flex-1 flex-col items-center justify-start p-4`}>
+									<Modal.Actions className=" flex h-auto w-full cursor-pointer justify-start">
+										<Modal.Action
+											className="rounded-full bg-muted/70 p-1 hover:bg-muted/60"
+											onClick={() => {
+												setShowDetails(false), setActive(false);
+											}}
+										>
+											<Modal.Icon icon={BiChevronLeft} size={32}></Modal.Icon>
+										</Modal.Action>
+									</Modal.Actions>
+									<div>asdasdasd</div>
+								</div>
+							</Modal.Content>
+						</Modal.Root>
+					</Backdrop>
+				)}
+			</AnimatePresence>
 		</div>
 	);
 }
