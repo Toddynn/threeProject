0781853fb40a6d6da4ff97a1b8a6e4935fd49589
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2023-09-26 18:00:01 -0300

    feat::sparkles: Create Product

---
+++ src/components/Product/index.tsx
@@ -0,0 +1,19 @@
+import ProductAction from './ProductAction';
+import ProductActions from './ProductActions';
+import ProductContent from './ProductContent';
+import ProductDescription from './ProductDescription';
+import ProductImage from './ProductImage';
+import ProductName from './ProductName';
+import ProductPrice from './ProductPrice';
+import ProductRoot from './ProductRoot';
+
+export const Product = {
+	Root: ProductRoot,
+	Content: ProductContent,
+	Image: ProductImage,
+	Name: ProductName,
+	Description: ProductDescription,
+	Price: ProductPrice,
+	Actions: ProductActions,
+	Action: ProductAction,
+};
