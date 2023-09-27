3d16bcb38be5cf4b87b38040f3535c4bc64b6986
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2023-09-27 08:42:24 -0300

    fix::poop: Correct Props Modal Root

--- src/components/Modal/ModalRoot.tsx
+++ src/components/Modal/ModalRoot.tsx
@@ -1,14 +1,12 @@
 import { MotionProps, motion } from 'framer-motion';
-import { HTMLAttributes, ReactNode } from 'react';
+import { HTMLAttributes } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-interface ModalRootProps extends HTMLAttributes<HTMLDivElement> {
-	children: ReactNode;
-}
+interface ModalRootProps extends HTMLAttributes<HTMLDivElement> {}
 
 export type CombinedModalRootProps = ModalRootProps & MotionProps;
 
-export default function ModalRoot({ children, className, animate, exit, initial, transition, variants, ...rest }: CombinedModalRootProps) {
+export default function ModalRoot({ children, className, ...rest }: CombinedModalRootProps) {
 	return (
 		<motion.div className={twMerge('relative z-50 ', className)} {...rest}>
 			{children}
