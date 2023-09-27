36ac903bbb2d8fb007d5f8047c58637980ff9c8a
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2023-09-27 16:03:57 -0300

    feat::sparkles: Add Vitor Model and animations

--- src/components/Objects/Door/index.tsx
+++ src/components/Objects/Door/index.tsx
@@ -1,8 +1,10 @@
-import { Environment, useGLTF } from '@react-three/drei';
+import { useAnimations, useCursor } from '@react-three/drei';
 import { PresetsType } from '@react-three/drei/helpers/environment-assets';
-import { Object3DNode } from '@react-three/fiber';
-import { useRef } from 'react';
+import { Object3DNode, useLoader } from '@react-three/fiber';
+import { useEffect, useRef, useState } from 'react';
 import { Group } from 'three';
+import { GLTFLoader } from 'three-stdlib';
+import { useAnimation } from '../../../hooks/useAnimation';
 
 export interface TableProps extends Object3DNode<Group, typeof Group> {
 	preset?: PresetsType;
@@ -12,12 +14,37 @@ export interface TableProps extends Object3DNode<Group, typeof Group> {
 
 /* const ANIM_SPEED = 12; */
 
-export default function Door({ preset = 'night', scale, position, ...rest }: TableProps) {
-	const groupRef = useRef<Group>(null);
+export default function Door({ preset = 'sunset', scale, position, ...rest }: TableProps) {
+	const doorRef = useRef<Group>(null);
+	const marcoRef = useRef<Group>(null);
 
-	const { nodes, materials } = useGLTF('/models/door/Door.gltf');
+	const { nodes, materials, animations } = useLoader(GLTFLoader, '/models/door/Frizzata.glb');
 
-	console.log(nodes, materials);
+	const doorAnimations = useAnimation();
+	const door = useAnimations(animations, doorRef);
+
+	useEffect(() => {
+		doorAnimations.setAnimations(door.names);
+	}, [door.names]);
+
+	const [isHovered, setIsHovered] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (doorAnimations.animationIndex === undefined) return;
+
+		const doorAnimationAction = door.actions[door.names[doorAnimations.animationIndex]];
+
+		if (doorAnimationAction) {
+			doorAnimationAction.fadeIn(0.5).play();
+			return () => {
+				doorAnimationAction.fadeOut(0.5).play();
+			};
+		}
+	}, [doorAnimations.animationIndex]);
+
+	useCursor(isHovered);
+
+	/* const { actions } = useAnimations(animations, groupRef); */
 
 	/* const { legs, legsColor, plateColor, tableWidth, legsMaterial, setLegsMaterial, plateMaterial, setPlateMaterial } = useConfigurator();
 
@@ -80,9 +107,8 @@ export default function Door({ preset = 'night', scale, position, ...rest }: Tab
 				);
 		}
 	};
- */
-	return (
-		<group {...rest} ref={groupRef} dispose={null} scale={scale} position={position}>
+
+     <group {...rest} ref={groupRef} dispose={null} scale={scale} position={position}>
 			<ambientLight intensity={0.2} />
 			<Environment preset={preset} />
 			<group position={[-0.062, 1.038, -0.006]}>
@@ -98,7 +124,25 @@ export default function Door({ preset = 'night', scale, position, ...rest }: Tab
 				<mesh geometry={nodes['G-__555591_2'].geometry} material={materials.Dobradiça} />
 			</group>
 		</group>
+
+     */
+
+	return (
+		<group {...rest} dispose={null} position={position} onPointerEnter={() => setIsHovered(true)} onPointerLeave={() => setIsHovered(false)}>
+			<group name="Scene">
+				<group name="PortaFrizzata" position={[0.006, 0, -0.401]} rotation={[Math.PI, 0, Math.PI]} scale={-1} ref={doorRef}>
+					<mesh name="G-__555564" geometry={nodes['G-__555564'].geometry} material={materials.Porta} />
+					<mesh name="G-__555564_1" geometry={nodes['G-__555564_1'].geometry} material={materials.Frizo} />
+					<mesh name="G-__555564_2" geometry={nodes['G-__555564_2'].geometry} material={materials.Parafuso} />
+					<mesh name="G-__555564_3" geometry={nodes['G-__555564_3'].geometry} material={materials.Dobradica} />
+				</group>
+				<group name="MarcoPFrizzata" rotation={[Math.PI, 0, Math.PI]} scale={[-1.009, -1, -0.962]} ref={marcoRef}>
+					<mesh name="G-__555627" geometry={nodes['G-__555627'].geometry} material={materials.Porta} />
+					<mesh name="G-__555627_1" geometry={nodes['G-__555627_1'].geometry} material={materials.Dobradica} />
+					<mesh name="G-__555627_2" geometry={nodes['G-__555627_2'].geometry} material={materials.Parafuso} />
+					<mesh name="G-__555627_3" geometry={nodes['G-__555627_3'].geometry} material={materials.Material} />
+				</group>
+			</group>
+		</group>
 	);
 }
-
-useGLTF.preload('/models/door/Door.gltf');
