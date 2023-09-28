b208e1e51705c76bec0945138058caed2a8c4cab
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2023-09-28 17:56:10 -0300

    feat::sparkles: Create spotLights in experience

--- src/components/Experiences/ExperienceDoor/index.tsx
+++ src/components/Experiences/ExperienceDoor/index.tsx
@@ -1,10 +1,11 @@
-import { CameraControls } from '@react-three/drei';
+import { CameraControls, OrbitControls, useHelper } from '@react-three/drei';
 import { PresetsType } from '@react-three/drei/helpers/environment-assets';
 import { useThree } from '@react-three/fiber';
+import { useControls } from 'leva';
 import { useEffect, useRef } from 'react';
+import { SpotLight, SpotLightHelper } from 'three';
 import { Vector3 } from 'three/src/math/Vector3.js';
 import Door from '../../Objects/Door';
-import { Portal } from '../../Portal';
 
 interface ExperienceDoorProps {
 	active: boolean;
@@ -28,9 +29,27 @@ export function ExperienceDoor({ active, portalArgs, name, preset }: ExperienceD
 		}
 	}, [active]);
 
+	const spotLightRef = useRef<SpotLight>(null!);
+	const secondSpotLightRef = useRef<SpotLight>(null!);
+
+	useHelper(spotLightRef, SpotLightHelper, 'red');
+	useHelper(secondSpotLightRef, SpotLightHelper, 'red');
+
+	const { distance, intensity, angle, x, y, z } = useControls({
+		intensity: { value: 37, min: 0, max: 100 },
+		distance: { value: 5, min: 0, max: 100 },
+		angle: { value: 0.42, min: 0.1, max: 1 },
+		x: { value: -1.9, min: -5, max: 5 },
+		y: { value: 1.5, min: -5, max: 5 },
+		z: { value: -2, min: -5, max: 5 },
+	});
+
 	return (
 		<>
-			<Portal
+			<spotLight ref={spotLightRef} position={[x, y, z]} distance={distance} intensity={intensity} angle={angle} />
+			<spotLight ref={secondSpotLightRef} position={[-x, y, -z]} distance={distance} intensity={intensity} angle={angle} />
+			{/* <Environment preset="sunset" ground /> */}
+			{/* <Portal
 				portalArgs={portalArgs}
 				name={name}
 				active={active}
@@ -39,9 +58,10 @@ export function ExperienceDoor({ active, portalArgs, name, preset }: ExperienceD
 				position={[0, 0, 0]}
 				preset={preset}
 			>
-				<Door scale={1} preset={preset} position={[0, -1, 0]} />
-			</Portal>
-			<CameraControls enabled={active} ref={cameraControlsRef} makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2} maxDistance={16} />
+			</Portal> */}
+			<Door scale={1} preset={preset} position={[0, 0, 0]} />
+			<OrbitControls enablePan={false} minPolarAngle={0} maxPolarAngle={Math.PI / 2} maxDistance={22} minDistance={3} />
+			{/* <CameraControls enabled={active} ref={cameraControlsRef} makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2} maxDistance={16} /> */}
 		</>
 	);
 }
