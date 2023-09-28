1c2f2e8ac0d853adda439907f7f47c256732cedc
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2023-09-28 17:55:42 -0300

    feat::sparkles: add button stop animation

--- src/components/Controls/ControlAnimations/index.tsx
+++ src/components/Controls/ControlAnimations/index.tsx
@@ -2,19 +2,19 @@ import { useAnimation } from '../../../hooks/useAnimation';
 import { Button } from '../../ShadCN/ui/button';
 
 export function ControlAnimations() {
-	const { animations, ...rest } = useAnimation();
+	const { animations, setAnimationIndex } = useAnimation();
 	return (
 		<>
 			{animations && (
 				<div className="fixed right-5 top-5 z-50 flex h-auto w-auto flex-col gap-3 overflow-y-auto rounded-md bg-white">
 					{animations.map((animation, index) => {
 						return (
-							<Button key={index} onClick={() => rest.setAnimationIndex(index)}>
+							<Button key={index} onClick={() => setAnimationIndex(index)}>
 								{animation}
 							</Button>
 						);
 					})}
-					<Button onClick={() => rest.setAnimationIndex(undefined)} variant={'destructive'}>
+					<Button onClick={() => setAnimationIndex(undefined)} variant={'secondary'}>
 						Parar
 					</Button>
 				</div>
