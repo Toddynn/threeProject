eecb95eba6361ee4694ca8a74ddc4abe2c0c31cc
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2023-09-29 17:20:48 -0300

    feat::sparkles: add toggle & tooltip

---
+++ src/components/ShadCN/ui/toggle.tsx
@@ -0,0 +1,39 @@
+import * as TogglePrimitive from '@radix-ui/react-toggle';
+import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from 'react';
+import { cn } from '../lib/utils';
+
+const toggleVariants = cva(
+	`inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors 
+     hover:bg-muted hover:text-muted-foreground 
+     focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring 
+     disabled:pointer-events-none disabled:opacity-50 
+     data-[state=on]:bg-muted-foreground data-[state=on]:text-accent data-[state=on]:hover:bg-muted data-[state=on]:hover:text-accent-foreground`,
+	{
+		variants: {
+			variant: {
+				default: 'bg-transparent border border-input',
+				outline: 'border border-input bg-muted shadow-sm hover:bg-muted-foreground hover:text-accent',
+				active: 'border border-input bg-muted-foreground shadow-sm hover:bg-muted hover:text-accent-foreground',
+			},
+			size: {
+				default: 'h-9 px-3',
+				sm: 'h-8 px-2',
+				lg: 'h-10 px-3',
+			},
+		},
+		defaultVariants: {
+			variant: 'default',
+			size: 'default',
+		},
+	},
+);
+
+const Toggle = React.forwardRef<
+	React.ElementRef<typeof TogglePrimitive.Root>,
+	React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> & VariantProps<typeof toggleVariants>
+>(({ className, variant, size, ...props }, ref) => <TogglePrimitive.Root ref={ref} className={cn(toggleVariants({ variant, size, className }))} {...props} />);
+
+Toggle.displayName = TogglePrimitive.Root.displayName;
+
+export { Toggle, toggleVariants };
