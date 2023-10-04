e320091e2c672c4e4092d04f856a706cc2a88d59
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2023-10-04 16:35:26 -0300

    feat::sparkles: AmbientAudio in Door

--- src/components/Objects/Door/index.tsx
+++ src/components/Objects/Door/index.tsx
@@ -4,21 +4,26 @@ import { Object3DNode, useLoader } from '@react-three/fiber';
 import { useEffect, useRef, useState } from 'react';
 import { Group } from 'three';
 import { GLTFLoader } from 'three-stdlib';
+import { useAmbientAudio } from '../../../hooks/useAmbientAudio';
 import { useAnimation } from '../../../hooks/useAnimation';
+import { useAudio, useBackgroundAudio } from '../../../hooks/useAudio';
 
 export interface TableProps extends Object3DNode<Group, typeof Group> {
 	preset?: PresetsType;
 	scale: number;
 	position?: [number, number, number];
+	rotation?: [number, number, number];
 }
 
-export default function Door({ preset = 'sunset', scale, position, ...rest }: TableProps) {
+export default function Door({ preset = 'sunset', scale, position, rotation, ...rest }: TableProps) {
 	const containerRef = useRef<Group>(null);
 	const doorRef = useRef<Group>(null);
+	const [volumeAmbientAudio, setVolumeAmbientAudio] = useState<number>(0.09);
 
-	const { animations, scene } = useLoader(GLTFLoader, '/models/door/PORTAO.glb');
+	const { animations, scene } = useLoader(GLTFLoader, '/models/door/PESADA.glb');
 
 	const doorAnimations = useAnimation();
+	const ambientAudio = useAmbientAudio();
 	const door = useAnimations(animations, doorRef);
 
 	useEffect(() => {
@@ -27,18 +32,36 @@ export default function Door({ preset = 'sunset', scale, position, ...rest }: Ta
 
 	const [isHovered, setIsHovered] = useState<boolean>(false);
 
+	const doorOpeningAudio = useAudio('audios/openingDoor.mp3');
+	const doorClosingAudio = useAudio('audios/closingDoor.mp3');
+	const backgroundAudio = useBackgroundAudio(ambientAudio.ambientAudios[ambientAudio.ambientAudioIndex!]?.path, volumeAmbientAudio);
+	const doorAnimationAction = door.actions[door.names[doorAnimations.animationIndex!]];
+
 	useEffect(() => {
-		const doorAnimationAction = door.actions[door.names[doorAnimations.animationIndex!]];
 		if (doorAnimationAction) {
 			doorAnimationAction.loop = 2200;
 			doorAnimationAction.clampWhenFinished = true;
 			doorAnimationAction.reset().fadeIn(0.5).play();
 
+			setVolumeAmbientAudio(1);
+			doorOpeningAudio();
 			return () => {
 				doorAnimationAction.fadeOut(0.5).play();
+				doorClosingAudio();
+				setVolumeAmbientAudio(0.09);
 			};
 		}
 	}, [doorAnimations.animationIndex]);
+
+	useEffect(() => {
+		if (!backgroundAudio) return;
+		backgroundAudio.play();
+		backgroundAudio.volume(volumeAmbientAudio);
+
+		return () => {
+			backgroundAudio.pause();
+		};
+	}, [ambientAudio.ambientAudioIndex, volumeAmbientAudio]);
 
 	useCursor(isHovered);
 
@@ -49,7 +72,7 @@ export default function Door({ preset = 'sunset', scale, position, ...rest }: Ta
 			receiveShadow
 			dispose={null}
 			position={position}
-			rotation={[0, 110, 0]}
+			rotation={rotation}
 			ref={containerRef}
 			onPointerEnter={() => setIsHovered(true)}
 			onPointerLeave={() => setIsHovered(false)}
