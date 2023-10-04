c7c2d06742346272adf77810f11fdd824a80c5c8
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2023-10-04 16:34:00 -0300

    feat::sparkles: New fction GoBack modalPreviewGltf

--- src/components/Modal/ModalPreviewGLTF/index.tsx
+++ src/components/Modal/ModalPreviewGLTF/index.tsx
@@ -2,11 +2,10 @@ import { Loader, useProgress } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Dispatch, SetStateAction, Suspense } from 'react';
 import { BiChevronLeft } from 'react-icons/bi'; // Import the fullscreen exit icon
-import { RiFullscreenExitLine, RiFullscreenLine } from 'react-icons/ri';
 import { Modal } from '..';
 import { SlideLeft } from '../../../constants/animation';
-import { Button } from '../../Button';
-import { ControlAnimations } from '../../Controls/ControlAnimations';
+import { ControlDoor } from '../../Controls/ControlDoor';
+import ControlPresets from '../../Controls/ControlPresets';
 import { ExperienceDoor } from '../../Experiences/ExperienceDoor';
 
 export interface ModalPreviewGltfProps {
@@ -20,20 +19,18 @@ export interface ModalPreviewGltfProps {
 export default function ModalPreviewGltf({ active, setActive, isFullScreen, setIsFullScreen }: ModalPreviewGltfProps) {
 	const { loaded } = useProgress();
 
-	const toggleFullscreen = () => {
-		setIsFullScreen((prev) => !prev);
+	const goBack = () => {
+		if (isFullScreen) {
+			setIsFullScreen(false);
+		}
+		return setActive(false);
 	};
 
 	return (
-		<Modal.Root onClick={(e: any) => e.stopPropagation()} variants={SlideLeft} initial="hidden" animate="visible" exit="exit" className=" h-full w-full">
+		<Modal.Root onClick={(e: any) => e.stopPropagation()} variants={SlideLeft} initial="hidden" animate="visible" exit="exit" className="h-full w-full">
 			{loaded && (
 				<Modal.Content>
-					<Modal.Action
-						className=" absolute left-5 top-5 z-50 rounded-full bg-muted/70 p-1 hover:bg-muted/60"
-						onClick={() => {
-							setActive(false);
-						}}
-					>
+					<Modal.Action className=" absolute left-5 top-5 z-50 rounded-full bg-muted/70 p-1 hover:bg-muted/60" onClick={goBack}>
 						<Modal.Icon icon={BiChevronLeft} size={32}></Modal.Icon>
 					</Modal.Action>
 
@@ -47,18 +44,8 @@ export default function ModalPreviewGltf({ active, setActive, isFullScreen, setI
 							<ExperienceDoor name="door" active={active} preset="sunset" portalArgs={[10, 10, 10]} />
 						</Suspense>
 					</Canvas>
-					<ControlAnimations />
-					<Modal.Actions className="absolute bottom-0 p-7">
-						<Modal.Action>
-							<Button.Root
-								data-active={isFullScreen}
-								className="rounded-full bg-muted p-2 hover:bg-muted-foreground hover:text-accent data-[active=true]:bg-muted-foreground data-[active=true]:text-accent"
-								onClick={toggleFullscreen}
-							>
-								<Button.Icon icon={isFullScreen ? RiFullscreenExitLine : RiFullscreenLine} size={20} />
-							</Button.Root>
-						</Modal.Action>
-					</Modal.Actions>
+					<ControlDoor isFullScreen={isFullScreen} setIsFullScreen={setIsFullScreen} />
+					<ControlPresets />
 				</Modal.Content>
 			)}
 			<Loader />
